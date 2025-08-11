8efcc808fd29f6c516612f7032a43e01ccaa0ae1
Darwin <jackdarwinbot@gmail.com>
2025-08-11 16:54:41 -0600

    Cuarto comit, Ingreso con Validaciones

--- Actividad15Progra4.py
+++ Actividad15Progra4.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 while True:
     print("1. Ingreso de datos")
     print("2. Mostrar datos")
@@ -10,17 +11,43 @@ while True:
     match opcion:
         case 1:
             print("Ingreso de datos")
-            nombre=input("Ingrese un nombre y u apellido(ejem, Juan Perez) ")
-            edad=int(input("Ingrese su edad: "))
-            fechanac=int("Ingrese su fecha de nacimineto... dd/mm/aaaa")
+            nombre = input("Ingrese su nombre y apellido (ejem: Juan Perez): ").strip()
+            if nombre == "":
+                print("El nombre no puede estar vacío, por favor Ingrese un Numero entero.")
+            elif len(nombre.split()) < 2:
+                print("Debe ingresar al menos un nombre y un apellido.")
+            else:
+                break
+            while True:
+                edadIngresada = input("Ingrese su edad: ")
+                if not edadIngresada.isdigit():
+                    print("La edad debe ser un número entero Positivo.")
+                else:
+                    edad = int(edadIngresada)
+                    if edad <= 0 or edad > 120:
+                        print("La edad debe estar entre 1 y 100.")
+                    else:
+                        break
+            while True:
+                fechanac = input("Ingrese su fecha de nacimiento (dd/mm/aaaa): ").strip()
+                try:
+                    convertirFecha = datetime.strptime(fechanac, "%d/%m/%Y")
+                    hoy = datetime.now()
+                    if convertirFecha> hoy:
+                        print("La fecha no puede ser en el futuro")
+                    elif convertirFecha.year < 1925:
+                        print("El año debe ser mayor o igual a 1925")
+                    else:
+                        break
+                except ValueError:
+                    print("Dato Invalido por favor Use el formato dd/mm/aaaa.")
             datos = [nombre, edad, fechanac]
-
             print("Exitoso...Datos ingresados correctamente:")
+        case 2:
+            print("Mostara Datos")
             print(f"Nombre completo: {datos[0]}")
             print(f"Edad: {datos[1]}")
             print(f"Fecha de nacimiento: {datos[2]}")
-        case 2:
-            print("")
         case 3:
             print("")
         case 4:
